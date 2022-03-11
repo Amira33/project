4160ad3cfc83556f736491d2e39a550fd28897c2
Amira33 <https://github.com/Amira33/file.git>
2022-03-12 01:15:27 +0200

    decliration

--- src/com/sprints/sample/Main.java
+++ src/com/sprints/sample/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	// write your code here
 
-                int num1, num2, sum;
+                int num1, num2, sum; // decliration
                 Scanner sc = new Scanner(System.in);
                 System.out.println("Enter First Number: ");
                 num1 = sc.nextInt();
